4574f840483341b047a1a7c925fd48a3fc9e2baf
Serge Voilokov <serge-v@users.noreply.github.com>
2020-10-23 19:24:42 -0400

    Update start.sh

--- cmd/etv/start.sh
+++ cmd/etv/start.sh
@@ -1,3 +1,3 @@
 echo startup file loaded
 source /home/alarm/params.sh
-omxplayer --win '0 0 320 240' --dbus_name org.mpris.MediaPlayer2.player1 ${URL}1.mp4 &
+omxplayer --win '0 0 320 240' --dbus_name org.mpris.MediaPlayer2.omxplayer2 ${URL}1.mp4 &
